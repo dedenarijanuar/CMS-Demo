d7e139c9f2408ce9634b91af074c31759aaea8d7
Yulianto <yuli.anto@icube-mobile.com>
2021-09-02 16:25:15 +0700

    Bug fix product page preview

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -2,14 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { ProductPostTemplate } from '../../templates/product-post'
 
-const ProductPagePreview = ({ entry, widgetFor, getAsset}) => {
-  const data = entry.getIn(['data']).toJS()
+const ProductPagePreview = ({ entry, widgetFor}) => {
 
   return (
     <ProductPostTemplate
       content={widgetFor('body')}
       description={entry.getIn(['data', 'description'])}
-      image={getAsset(data.image)}
       title={entry.getIn(['data', 'title'])}
     />
   )
